81d0a67119f5b0211e46bf7862bebb642f1ecb88
Denisse Lara <denisselarav@gmail.com>
2019-02-04 23:48:30 -0300

    final

--- wp-content/themes/restaurant-theme/header.php
+++ wp-content/themes/restaurant-theme/header.php
@@ -1,42 +1,42 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-  <head>
-    <meta charset="<?php bloginfo('charset') ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php bloginfo('IL FONTI DI MODENA'); ?></title>
-    <?php wp_head() ?>
-  </head>
-<body>
+  <!DOCTYPE html>
+  <html <?php language_attributes(); ?>>
+    <head>
+      <meta charset="<?php bloginfo('charset') ?>">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title><?php bloginfo('IL FONTI DI MODENA'); ?></title>
+      <?php wp_head() ?>
+    </head>
+  <body>
 
-  <div class="gtco-loader"></div>
+    <div class="gtco-loader"></div>
 
-  <div id="page">
-  <!-- navegación-->
-    <nav class="gtco-nav" role="navigation">
-      <div class="gtco-container fixed">
+    <div id="page">
+    <!-- navegación-->
+      <nav class="gtco-nav" role="navigation">
+        <div class="gtco-container fixed"
 
-        <div class="row fondo_blanco">
-          <div class="col-sm-2 col-xs-10">
+          <div class="row fondo_blanco">
+            <div class="col-sm-2 col-xs-10">
 
 
-            <div id="gtco-logo"> <a href="index.html"><img src=" <?php echo get_template_directory_uri(); ?>/assets/img/marca/logo.png" alt="logo" class="img-responsive"></div>
+              <div id="gtco-logo"> <a href="index.html"><img src=" <?php echo get_template_directory_uri(); ?>/assets/img/marca/logo.png" alt="logo" class="img-responsive"></div>
+            </div>
+
+
+
+
+          <div class="col-xs-10 col-xs-2 text-right menu-1 padding_right">
+              <ul>
+                <li><a href="#carousel">Home</a></li>
+                <li>
+                  <a href="#carta">La Carta</a>
+                </li>
+
+                <li><a href="#reserva">Reservas</a></li>
+                <li><a href="#gtco-footer">Contacto</a></li>
+              </ul>
+            </div>
           </div>
 
-
-
-
-        <div class="col-xs-10 col-xs-2 text-right menu-1 padding_right">
-            <ul>
-              <li><a href="#carousel">Home</a></li>
-              <li>
-                <a href="#carta">La Carta</a>
-              </li>
-
-              <li><a href="#reserva">Reservas</a></li>
-              <li><a href="#gtco-footer">Contacto</a></li>
-            </ul>
-          </div>
         </div>
-
-      </div>
-    </nav>
+      </nav>
